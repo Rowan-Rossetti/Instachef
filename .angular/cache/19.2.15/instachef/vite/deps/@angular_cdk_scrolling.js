5440b8494070292f558e7e32ed941080ce28dbbb
RoRo141414 <146880906+RoRo141414@users.noreply.github.com>
2026-02-05 16:11:23 +0100

    Commit

---
+++ .angular/cache/19.2.15/instachef/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,47 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-ZM5LAZ6C.js";
+import "./chunk-2O4WY5GE.js";
+import "./chunk-UJKO3GMG.js";
+import {
+  Dir
+} from "./chunk-5IRT5ITM.js";
+import "./chunk-Z6KUTGQL.js";
+import "./chunk-LU6RDJ7A.js";
+import "./chunk-XLE5N5VE.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
